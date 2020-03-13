9fefe19d0b7f01db440377e707a83dd16649e12a
stasbar <stasbar1995@gmail.com>
2020-03-13 19:27:35 +0100

    add wall of public votings add page to cast a vote add page to create voting
    Took 31 seconds

---
+++ app/src/pages/voting/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface VotesProps {
+}
+
+const Votes: React.FC<VotesProps> = () => (
+  <div>
+    <p>
+      Public Votes
+    </p>
+  </div>
+);
+
+export default Votes;
