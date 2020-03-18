6e13f0a4ca957d92915a7bf33ed96e7d4ce2e476
stasbar <stasbar1995@gmail.com>
2020-03-18 01:55:52 +0100

    dockerize with mongo service add docker dev flavor
    Took 1 hour 13 minutes

--- server/src/database/models/index.ts
+++ server/src/database/models/index.ts
@@ -1,2 +1,10 @@
 export const VOTING = 'Voting';
 export const KEYCHAIN = 'Keychain';
+
+// Don't change order because of cyclic dependency
+import Voting from './Voting'
+import Keychain from './Keychain'
+
+export {
+  Voting, Keychain,
+}
