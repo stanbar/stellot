64c60f38d15a55f0e4f8985f53351aaa8d1ca1eb
stasbar <stasbar1995@gmail.com>
2020-03-20 03:57:02 +0100

    new UI
    Took 25 seconds

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
+import styled from "styled-components";
+import Wall from "@/pages/wall";
 import styles from './index.css';
-import { Typography, Button } from 'antd';
-import { Link } from "umi";
+
+const Title = styled.h1`
+font-size: 28px;
+font-weight: 300;
+`;
+const SubTitle = styled.h2`
+font-size: 36px;
+font-weight: 500;
+line-height: 1.33;
+`;
+const SubSubTitle = styled.p`
+font-size: 18px;
+line-height: 1.67;
+`;
 
 export default function () {
   return (
     <div className={styles.normal}>
       <ul className={styles.list}>
-        <Typography.Title>Welcome in Stellot - Voting platform backed by Stellar blockchain network</Typography.Title>
-        <li>
-          <Link to="/create"><Button type="primary" size="large">Create Vote</Button></Link>
-        </li>
-        <li>
-          <Link to="/wall"><Button type="dashed" size="large">Explore</Button></Link>
-        </li>
+        <Title>Welcome in Stellot</Title>
+        <SubTitle>Voting platform backed by Stellar blockchain network</SubTitle>
+        <SubSubTitle>Stellot was designed around voter privacy, ensuring that everyone can be sure about it&apos;s vote
+          anonimity. Inherit decentralized and permission-less Stellar blockchain properties, allowing everyone to
+          verify the election results without having to trust central authority.</SubSubTitle>
+        <Wall/>
       </ul>
     </div>
   );
