acbcab5c3975924b72d1c48a495107bea05b7028
stasbar <stasbar1995@gmail.com>
2020-04-05 01:39:44 +0200

    add keybase-auth cors support
    Took 6 minutes

--- keybase-auth/src/app.ts
+++ keybase-auth/src/app.ts
@@ -2,12 +2,14 @@ import express, { NextFunction, Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import errorhandler from 'errorhandler';
+import cors from 'cors';
 import router from './routes'
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json({ limit: '0.5mb' }));
 app.use(express.urlencoded({ limit: '0.5mb', extended: false }));
