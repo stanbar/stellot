6312e34a41976befacf3006868d55311c0d90686
stasbar <stasbar1995@gmail.com>
2020-04-15 16:55:13 +0200

    add FAQ title change Q/A backgrund to white
    Took 1 hour 11 minutes

---
+++ app/src/components/Title.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+font-size: 28px;
+font-weight: 300;
+`;
--- app/src/typings.d.ts
+++ app/src/typings.d.ts
@@ -1,4 +1,5 @@
 declare module '*.css';
+declare module "*.less";
 declare module '*.svg';
 declare module "*.png";
 
