241281d3cba85cf09aab16fc9b07947573e46c8b
stasbar <stasbar1995@gmail.com>
2020-05-02 02:26:31 +0200

    fix trust proxy
    Took 2 minutes

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const app = express();
 
-app.use('trust proxy', () => true)
+app.set('trust proxy', true)
 app.use(logger('dev'));
 app.use(express.json({ limit: '0.5mb' }));
 app.use(express.urlencoded({ limit: '0.5mb', extended: false }));
