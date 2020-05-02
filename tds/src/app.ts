de89f58aac3be9c367252abb73319f4ec8734b34
stasbar <stasbar1995@gmail.com>
2020-05-02 17:57:45 +0200

    rename server to tds

--- tds/src/app.ts
+++ tds/src/app.ts
@@ -1,9 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import crypto from 'crypto';
 import errorhandler from 'errorhandler';
-import path from 'path';
 import router from './routes'
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -15,10 +13,6 @@ app.use(logger('dev'));
 app.use(express.json({ limit: '0.5mb' }));
 app.use(express.urlencoded({ limit: '0.5mb', extended: false }));
 app.use(cookieParser());
-if (!process.env.WEBAPP_DIR) {
-  throw new Error('Could not find webapp dir');
-}
-app.use(express.static(process.env.WEBAPP_DIR!));
 if (!isProduction) {
   app.use(errorhandler());
 }
@@ -32,26 +26,6 @@ app.get('/myIp', (req, res) => {
   res.send({ ip, ips, remoteAddress: req.connection.remoteAddress, forwarded, cloudFlareIp })
 });
 app.use(router);
-
-// Mock of Authorization Provider
-app.post('/api/login', (req, res) => {
-  const { login, password } = req.body;
-  if (!login || !password) {
-    return res.sendStatus(400).end();
-  }
-  return res
-    .json({
-      userId: crypto
-        .createHmac('sha256', process.env.ISSUE_SECRET_KEY!)
-        .update(login)
-        .digest('hex')
-        .substring(0, 16),
-    })
-    .end();
-});
-app.get('*', (req, res) => {
-  res.sendFile(path.join(process.env.WEBAPP_DIR!, '/index.html'));
-});
 
 export class HttpError extends Error {
   status: number;
