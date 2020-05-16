35b70ffe801531d267cdaa0828ca1fd7d0d683ae
stasbar <stasbar1995@gmail.com>
2020-05-16 19:24:36 +0200

    add toBuffer and more tests

--- crypto/src/__tests__/elGamal.test.ts
+++ crypto/src/__tests__/elGamal.test.ts
@@ -1,5 +1,6 @@
 import { createEncryptionKeypair, decodePublicKey, decodePrivateKey, ElGamal, DecryptionElGamal, EncryptionElGamal } from '../'
 import { rand } from 'elliptic'
+import { toBuffer } from '../elGamal/utils'
 
 describe('elGamal', () => {
     it('should return base64 priv and public keys', () => {
@@ -37,14 +38,33 @@ describe('elGamal', () => {
         const eg = ElGamal.fromPublicKey(pub.p.toString(), pub.g.toString(), pub.y.toString())
         expect(pub.y.toString()).toEqual(eg.y.toString())
     });
+    it('ciphertext should be 2 times keysize', () => {
+        Array.from({ length: 20 }, () => {
+            const keySizeBytes = 16
+            const { privateKey, publicKey } = createEncryptionKeypair(keySizeBytes * 8)
+            const pub = decodePublicKey(publicKey)
+            const priv = decodePrivateKey(privateKey)
 
-    it('should encrypt and decrypt random 32 byte memo with elgamal', () => {
+            const randomMemo: Buffer = Buffer.from(rand(keySizeBytes));
+            randomMemo.writeUInt8(1, 0);
+            const randomMemoAscii = randomMemo.toString('ascii')
+
+            const encrypter = ElGamal.fromPublicKey(pub.p.toString(), pub.g.toString(), pub.y.toString())
+            const cipher = encrypter.encrypt(randomMemoAscii)
+
+            expect(cipher.a.bitLength()).toBeLessThanOrEqual(keySizeBytes * 8)
+            expect(cipher.b.bitLength()).toBeLessThanOrEqual(keySizeBytes * 8)
+            expect(toBuffer(cipher.a).length).toBeLessThanOrEqual(keySizeBytes)
+            expect(toBuffer(cipher.b).length).toBeLessThanOrEqual(keySizeBytes)
+        })
+    });
+    it('should encrypt and decrypt random 16 byte memo with elgamal', () => {
         Array.from({ length: 20 }, () => {
             const { privateKey, publicKey } = createEncryptionKeypair()
             const pub = decodePublicKey(publicKey)
             const priv = decodePrivateKey(privateKey)
 
-            const randomMemo: Buffer = Buffer.from(rand(28));
+            const randomMemo: Buffer = Buffer.from(rand(16));
             randomMemo.writeUInt8(1, 0);
             const randomMemoAscii = randomMemo.toString('ascii')
 
@@ -74,5 +94,21 @@ describe('elGamal', () => {
             const decrypted = decrypter.decrypt(cipher)
             expect(randomMemoAscii).toEqual(new Buffer(decrypted.toByteArray()).toString('ascii'))
         })
+    });
+    it('should encrypt and decrypt hello world 128bit elgamal', () => {
+        const { privateKey, publicKey } = createEncryptionKeypair(128)
+        const pub = decodePublicKey(publicKey)
+        const priv = decodePrivateKey(privateKey)
+
+        const helloWorld = 'Hello World!'
+        const helloWorldBuffer: Buffer = Buffer.from(helloWorld);
+        const helloWroldAscii = helloWorldBuffer.toString('ascii')
+
+        const encrypter = ElGamal.fromPublicKey(pub.p.toString(), pub.g.toString(), pub.y.toString())
+        const cipher = encrypter.encrypt(helloWroldAscii)
+
+        const decrypter: DecryptionElGamal = ElGamal.fromPrivateKey(priv.p.toString(), priv.g.toString(), priv.y.toString(), priv.x.toString())
+        const decrypted = decrypter.decrypt(cipher)
+        expect(helloWorld).toEqual(new Buffer(decrypted.toByteArray()).toString())
     });
 })
