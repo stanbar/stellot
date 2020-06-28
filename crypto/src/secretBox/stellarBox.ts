06ec69a94119457d560df834c825750859cf85d9
stasbar <stasbar1995@gmail.com>
2020-06-28 16:07:18 +0200

    add secret-memo code

---
+++ crypto/src/secretBox/stellarBox.ts
@@ -0,0 +1,29 @@
+import { Keypair, StrKey } from 'stellar-sdk';
+import {
+    encrypt as _encrypt,
+    decrypt as _decrypt
+} from '@futuretense/ed25519-box';
+
+export function encrypt(
+    keys: Keypair,
+    to: string,
+    input: Uint8Array,
+    nonce: Uint8Array | null,
+    authenticate: Boolean = true
+): Promise<Uint8Array> {
+    const privKey = keys.rawSecretKey();
+    const pubKey = StrKey.decodeEd25519PublicKey(to);
+    return _encrypt(privKey, pubKey, input, nonce, authenticate);
+}
+
+export function decrypt(
+    keys: Keypair,
+    from: string,
+    input: Uint8Array,
+    nonce: Uint8Array | null,
+    authenticate: Boolean = true
+): Promise<Uint8Array> {
+    const privKey = keys.rawSecretKey();
+    const pubKey = StrKey.decodeEd25519PublicKey(from);
+    return _decrypt(privKey, pubKey, input, nonce, authenticate);
+}
