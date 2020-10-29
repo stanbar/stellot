1079186e925282e9fc901accb1903458af1a543d
stasbar <stasbar1995@gmail.com>
2020-10-30 00:26:20 +0100

    remove signleton

--- standalone/src/blindsig/index.ts
+++
@@ -1,8 +0,0 @@
-import elliptic from 'elliptic';
-import SignerSession from './SignerSession'
-import VoterSession from './VoterSession'
-
-const EdDSA = elliptic.eddsa;
-const ed25519 = new EdDSA('ed25519');
-
-export { SignerSession, VoterSession, EdDSA, ed25519 }
