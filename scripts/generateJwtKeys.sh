5fc11b4a891998ef14d6864579d90017f3ade9f5
Stanislaw Baranski <stan@stan.bar>
2022-12-15 00:51:04 +0100

    generate EC keys

--- scripts/generateJwtKeys.sh
+++ scripts/generateJwtKeys.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-openssl genrsa -out jwt_rs256_priv.pem 4096
-openssl rsa -in jwt_rs256_priv.pem -pubout > jwt_rs256_pub.pem
+openssl ecparam -name prime256v1 -genkey -out jwt_rs256_priv.pem
+openssl ec -in jwt_rs256_priv.pem -pubout -out jwt_rs256_pub.pem
 
 cp jwt_rs256_priv.pem ../tds/
 cp jwt_rs256_priv.pem ../emails-auth/
